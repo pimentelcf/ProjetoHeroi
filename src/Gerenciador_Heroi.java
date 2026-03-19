b01a53284d023357f60ab067ab597f3ef8f9732d
ALUNO <vitor.pimentel.santos@escola.pr.gov.br>
2026-03-19 10:57:26 -0300

    conectado as classes na main falta ajustar a classe Gerenciador Heroi, pois ela ta com erro e não conectou a classe com a  main dar continuidade e resolver mais tarde assim que possivel, classes criadas falta passar os metodos e construtores em algumas classes como missaoHeroi e gerenciador Heroi.

--- src/Gerenciador_Heroi.java
+++ src/Gerenciador_Heroi.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Gerenciador_Heroi {
+
+
+    ArrayList<Heroi>ArrayList lista_herois new = ArrayList<>();
+
+    public void adicionar(Heroi h)  {
+        lista_herois.add(h);
+        System.out.println("testando");
+
+
+    }
+
+
 }
--- src/Heroi.java
+++ src/Heroi.java
@@ -1,2 +1,13 @@
 public class Heroi {
+     String nome;
+     int forca;
+
+
+     //criado o contrutor para conectar com a main
+     public Heroi(String nome, int forca) {
+         this.nome = nome;
+         this.forca = forca;
+
+     }
+
 }
