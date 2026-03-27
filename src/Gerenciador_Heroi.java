093f4464be132a8c39efcab5203656bd15947f0e
ALUNO <vitor.pimentel.santos@escola.pr.gov.br>
2026-03-27 10:59:41 -0300

    feito classe gerenciador heroi e alterado um pouco a main, possivelmente vai ter que refatorar a logica mais pra frente

--- src/Gerenciador_Heroi.java
+++ src/Gerenciador_Heroi.java
@@ -1,16 +1,21 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Gerenciador_Heroi {
+public class GerenciadorHeroi {
+    // ATRIBUTO: A "gaveta" que vai guardar o objeto Heroi
+    public Heroi heroiAtivo;
 
-
-    ArrayList<Heroi>ArrayList lista_herois new = ArrayList<>();
-
-    public void adicionar(Heroi h)  {
-        lista_herois.add(h);
-        System.out.println("testando");
-
-
+    // metodo adicionar o heroi da main
+    public void adicionarHeroi(Heroi heroiRecebido) {
+        this.heroiAtivo = heroiRecebido;
+        System.out.println("Gerenciador: Herói " + heroiAtivo.nome + " foi vinculado!");
     }
 
-
+    public void treinar() {
+        if (this.heroiAtivo != null) {
+            this.heroiAtivo.nivelForca += 10;
+            System.out.println("Treino concluído! Nova força de " + heroiAtivo.nome + ": " + heroiAtivo.nivelForca);
+        } else {
+            System.out.println("Erro: Nenhum herói vinculado para treinar!");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-   public class Main {
-       public static void main(String[] args) {
-           // Seu código de controle vai aqui dentro
+public class Main {
+    public static void main(String[] args) {
+        // 1. Criamos o objeto do Herói (O DNA)
+        Heroi h1 = new Heroi();
+        h1.nome = "Arqueiro Verde";
+        h1.superPoder = "Pontaria";
+        h1.nivelForca = 50;
 
-           Gerenciador_Heroi gerenciado = new Gerenciador_Heroi();
-           Heroi h1 = new Heroi("Pimentel", 20);
+        // 2. Criamos o Gerenciador (A Máquina)
+        GerenciadorHeroi gerenciador = new GerenciadorHeroi();
 
-           //  Usar o nome da variável (gerenciador)
-           // para chamar o método adicionar;
-           gerenciado.adicionar(h1);
-       }
+        // Passamos o objeto 'h1' para dentro do metodo gerenciador
+        // É aqui que o 'this.heroiAtivo = heroiRecebido' acontece!
+        gerenciador.adicionarHeroi(h1);
 
+        // O gerenciador agora consegue treinar o h1 porque ele o "conhece"
+        gerenciador.treinar();
+
+        // Se pedirmos para o herói se exibir, veremos que a força subiu
+        h1.exibirFicha();
+    }
 }
